90d2916215eb9568117673615e5be1108f02ef41
jacksonnorr <jacksonnorr@gmail.com>
2022-03-11 15:32:48 -0700

    added more info for each coin pulled in, updated styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,12 +14,24 @@ xhr.onreadystatechange = function () {
 xhr.send();
 
 function formatData (data) {
+    var contentArea = $('.tickerWrapper');
+    contentArea.innerHTML = '';
     // console.log(JSON.parse(data[0]).id);
     parseData = JSON.parse(data.responseText)
     console.log(parseData)
     parseData.forEach((value, index) => {
         if (index < 50) {
-            console.log(value.id)
+            var mktCap = formatMktCap(value.market_cap);
+            contentArea.append(`<div class='ticker'>${value.name}: $${value.current_price.toLocaleString('en-US')} Market Cap: $${mktCap} 24hr change: ${value.price_change_percentage_24h.toFixed(2)}%</div>`)
         }
     }
 )}
+function formatMktCap (val) {
+    if (val > 1000000000) {
+        return (val/1000000000).toFixed(2) + "B";
+    }
+    else if (val > 1000000) {
+        return (val/1000000).toFixed(2) + "M";
+    }
+    else return val;
+}
